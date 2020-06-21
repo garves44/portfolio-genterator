36f92980420ca2fecd3c8801f12f52fc613b78e6
garves44 <garves44@gmail.com>
2020-06-21 16:06:31 -0600

    added stylessheet refactor code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
-const fs = require('fs');
+const {writeFile, copyFile} = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -136,13 +136,19 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('index.html', pageHTML, err =>{
-        if (err) throw err;
-
-        console.log("Portfolio complete! Checkout index.html to see the output!");
-    });
-    
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
+    
